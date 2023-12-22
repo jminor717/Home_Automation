0a4649f6ccb9d2ca3a7614a04af5bcf9ae765b4c
jake minor <jminor1@ycp.edu>
2023-12-21 20:18:33 -0500

    testing i2c coms with epaper

--- my_components/Segmented_Epaper.h
+++ my_components/Segmented_Epaper.h
@@ -4,21 +4,47 @@
 using namespace i2c;
 
 #define EPD_RST 19
+static const char* const TAG = "Segmented_E_paper";
 
-typedef void (*callback_function)(void);
+typedef void (*callback_function)(I2CBus* bussin);
 
 struct DisplayAction {
     callback_function function; // Function pointer
     uint16_t delayAfter; // time to wait after this action before the next is performed
 };
 
-void init_Display(void)
+uint8_t Current_EPD_Temperature_Compensation = 25;
+
+void EPD_Reset_1(I2CBus* bussin) { digitalWrite(EPD_RST, 1); }
+void EPD_Reset_2(I2CBus* bussin) { digitalWrite(EPD_RST, 0); }
+void EPD_Reset_3(I2CBus* bussin) { digitalWrite(EPD_RST, 1); }
+void EPD_PowerOn(I2CBus* bussin) { bussin->write(adds_com, new uint8_t[1] { 0x2B }, 1); } // POWER_ON
+void EPD_Boost(I2CBus* bussin) { bussin->write(adds_com, new uint8_t[2] { 0xA7, 0xE0 }, 2); } // boost, TSON
+void EPD_Temperature1(I2CBus* bussin)
 {
+    bussin->write(adds_com, new uint8_t[3] { 0x7E, 0x81, 0xB4 }, 3);
 }
+void EPD_Temperature2(I2CBus* bussin)
+{
+    uint8_t data[] = { 0xe7, 0 }; // Set default frame time
+    if (Current_EPD_Temperature_Compensation < 5)
+        data[1] = 0x31; // 0x31  (49+1)*20ms=1000ms
+    else if (Current_EPD_Temperature_Compensation < 10)
+        data[1] = 0x22; // 0x22  (34+1)*20ms=700ms
+    else if (Current_EPD_Temperature_Compensation < 15)
+        data[1] = 0x18; // 0x18  (24+1)*20ms=500ms
+    else if (Current_EPD_Temperature_Compensation < 20)
+        data[1] = 0x13; // 0x13  (19+1)*20ms=400ms
+    else
+        data[1] = 0x0e; // 0x0e  (14+1)*20ms=300ms
+    bussin->write(adds_com, data, 2);
+}
+
+void AsyncDelay(I2CBus* bussin) { }
 
 class SegmentedEPaper : public Component, public i2c::I2CDevice {
     I2CBus* _i2cBus;
-    uint8_t VAR_Temperature = 20;
+
     DisplayAction actionQueue[32];
     uint8_t queueIndex = 0;
     uint8_t queueHead = 0;
@@ -41,24 +67,44 @@ class SegmentedEPaper : public Component, public i2c::I2CDevice {
 public:
     SegmentedEPaper(I2CBus* Bus) { _i2cBus = Bus; }
 
-    void setup() override
+    void Reset_Display(void)
     {
-        // This will be called once to set up the component
-        // think of it as the setup() call in Arduino
+        AddAction(EPD_Reset_1, 200);
+        AddAction(EPD_Reset_2, 20);
+        AddAction(EPD_Reset_3, 200);
+    }
+    void Init_Display(void)
+    {
+        ESP_LOGD(TAG, "HEY ##### ###### #### Display init started");
+        Reset_Display();
+        AddAction(EPD_PowerOn, 10);
+        AddAction(EPD_Boost, 10);
+        CompensateForTemperature(25);
+    }
+
+    void CompensateForTemperature(uint8_t currentTemp)
+    {
+        ESP_LOGD(TAG, "temp Compensation %d", currentTemp);
+        Current_EPD_Temperature_Compensation = currentTemp;
+        AddAction(EPD_Temperature1, 10);
+        AddAction(EPD_Temperature2, 10);
+    }
+
+    void setup() override
+    { // This will be called once to set up the component think of it as the setup() call in Arduino
         // set_i2c_address(0x44);
         pinMode(18, INPUT);
         pinMode(EPD_RST, OUTPUT);
-        AddAction(init_Display, 200);
-        AddAction(init_Display, 200);
+        Init_Display();
     }
 
     void loop() override
     { // This will be called very often after setup time. think of it as the loop() call in Arduino
         uint64_t currentTime = esp_timer_get_time();
-        if (queueLength > 0 && canRunNextActionAt > currentTime) {
-
+        if (queueLength > 0 && currentTime > canRunNextActionAt) {
             DisplayAction runningAction = actionQueue[queueIndex];
-            runningAction.function();
+            ESP_LOGD(TAG, "running action %d at %d, %d remaining, next action in %d", queueIndex, (currentTime / 1000), queueLength, runningAction.delayAfter);
+            runningAction.function(_i2cBus);
             canRunNextActionAt = esp_timer_get_time() + (runningAction.delayAfter * 1000);
             queueLength--;
             queueIndex = (queueIndex + 1) & queueModulus;
