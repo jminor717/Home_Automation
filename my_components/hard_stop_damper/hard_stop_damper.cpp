eac03d2cce2c013558c9d3c1f5abe5498dea5c24
Jacob Minor <jminor1@ycp.edu>
2025-01-28 16:21:52 -0500

    change difference calculation

--- my_components/hard_stop_damper/hard_stop_damper.cpp
+++ my_components/hard_stop_damper/hard_stop_damper.cpp
@@ -17,14 +17,12 @@ namespace hard_stop_damper {
         this->servo_control->write(startingPosition);
         delay(3000);
         float reachedPosition = this->v_servo_sensor->sample();
+        float lastReached = reachedPosition;
         float reachedMapped = startingPosition;
         float commandedPosition = startingPosition;
         while (true) {
             commandedPosition += increment;
-            if (commandedPosition > upperLimit) {
-                break;
-            }
-            if (commandedPosition < lowerLimit) {
+            if (commandedPosition > upperLimit || commandedPosition < lowerLimit){
                 break;
             }
             this->servo_control->write(commandedPosition);
@@ -32,13 +30,14 @@ namespace hard_stop_damper {
             reachedPosition = this->v_servo_sensor->sample();
             // reachedMapped = remap(reachedPosition, float(0.5), float(2.5), float(0.0), float(1.0));
             reachedMapped = remap(reachedPosition, float(0.0), float(3.3), float(0.0), float(1.0));
-            ESP_LOGI(TAG, "commanded: %f, reached: %f, mapped: %f, diff: %f", commandedPosition, reachedPosition, reachedMapped, abs(commandedPosition - reachedMapped));
-            if (abs(commandedPosition - reachedMapped) > 0.075) {
-                break;
+            ESP_LOGI(TAG, "commanded: %f, reached: %f, last: %f, mid dif: %f, mapped: %f, diff: %f", commandedPosition, reachedPosition, lastReached, abs(reachedPosition - lastReached), reachedMapped, abs(commandedPosition - reachedMapped));
+            if (abs(reachedPosition - lastReached) < abs(increment)) {
+                break; // when the distance moved gets small
             }
+            lastReached = reachedPosition;
         }
         Position pos;
-        pos.comand_position = reachedMapped;
+        pos.comand_position = commandedPosition - increment;
         pos.voltage_read = reachedPosition;
         return pos;
     }
@@ -53,12 +52,11 @@ namespace hard_stop_damper {
         auto ONE_ = local_this->move_to_hard_stop(+0.025, 0.5, 0, 1);
         ESP_LOGI(TAG, "stop 1: %f, %f", ONE_.voltage_read, ONE_.comand_position);
 
-
         if (local_this->open_at_center) {
-            if (local_this->switch_open_and_close){
+            if (local_this->switch_open_and_close) {
                 local_this->servo_lower_limit->value() = ONE_.voltage_read;
                 local_this->close_position->value() = ONE_.comand_position;
-            }else{
+            } else {
                 local_this->servo_lower_limit->value() = ZERO_.voltage_read;
                 local_this->close_position->value() = ZERO_.comand_position;
             }
