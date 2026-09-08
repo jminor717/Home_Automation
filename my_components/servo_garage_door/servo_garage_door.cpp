d9c9d0dd3fb84b8d1b01c0cd066f0fafc596ff89
Jacob Minor <jminor1@ycp.edu>
2026-09-08 18:22:52 -0400

    seim working high speed servo controller

--- my_components/servo_garage_door/servo_garage_door.cpp
+++ my_components/servo_garage_door/servo_garage_door.cpp
@@ -1,34 +1,166 @@
-#include "dc_relay.h"
+#include "servo_garage_door.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
+#include <algorithm>
 #include <driver/ledc.h>
 
 namespace esphome {
 namespace servo_garage_door {
-// copied from ledc_output.cpp
+    static const char *const TAG = "ServoGarageDoor";
+    void ServoGarageDoor::setup()
+    {
+        for (ServoMotor* motor : this->motors) {
+            motor->setup();
+        }
 
-    void Servo_Garage_Door::setup()
+        this->servo_event_queue = xQueueCreate(6, sizeof(ServoEvent));
+        //   xTaskCreatePinnedToCore(&ESP32Camera::framebuffer_task,
+        //                   "framebuffer_task",           // name
+        //                   FRAMEBUFFER_TASK_STACK_SIZE,  // stack size
+        //                   this,                         // task pv params
+        //                   1,                            // priority
+        //                   nullptr,                      // handle
+        //                   1                             // core
+        xTaskCreatePinnedToCore(ServoGarageDoor::backgroundServoControlTask, "servo_task", 8192 * 5, (void*)this, 19, &this->servo_task_handle, 0);
+
+                    // xTaskCreate(ServoGarageDoor::backgroundServoControlTask, "servo_task", 8192 * 5, (void*)this, 2, &this->servo_task_handle);
+    }
+
+    void ServoMotor::setup()
     {
 
-        this->servo_event_queue = xQueueCreate(4, sizeof(ServoEvent));
-        xTaskCreate(Servo_Garage_Door::backgroundServoControlTask, "servo_task", 8192 * 2, (void*)this, 2, &this->servo_task_handle);
+        this->step_pin->setup();
+        this->step_pin->pin_mode(gpio::FLAG_OUTPUT | gpio::FLAG_PULLDOWN);
+
+        this->dir_pin->setup();
+        this->dir_pin->pin_mode(gpio::FLAG_OUTPUT | gpio::FLAG_PULLDOWN);
+
+        if (this->enable_pin != nullptr) {
+            this->enable_pin->setup();
+            this->enable_pin->pin_mode(gpio::FLAG_OUTPUT | gpio::FLAG_PULLDOWN);
+            this->enable_pin->digital_write(0);
+        }
+
+
+    }
+
+    void ServoGarageDoor::move_servo(uint32_t steps, bool direction, uint32_t max_speed, uint32_t acceleration, uint32_t jerks)
+    {
+        ServoEvent event{};
+        event.steps = steps;
+        event.direction = direction;
+        event.max_speed = max_speed;
+        event.acceleration = acceleration;
+        event.jerks = jerks;
+        ESP_LOGI(TAG, "Enqueuing servo move event");
+
+        if (xQueueSendToBack(this->servo_event_queue, &event, 100 / portTICK_PERIOD_MS) != pdTRUE) {
+            ESP_LOGW(TAG, "Failed to enqueue servo move event");
+        }
+    }
+
+    void ServoGarageDoor::dump_config()
+    {
+        ESP_LOGCONFIG(TAG, "ServoGarageDoor reader:");
+    }
+
+    void ServoGarageDoor::backgroundServoControlTask(void* params)
+    {
+        ServoGarageDoor* this_controller = static_cast<ServoGarageDoor*>(params);
+
+        // 1 second after startup start running the servo control task
+        delay(1'000);
+
+        // run servo control logic in separate thread since it can take a long time to complete a servo movement
+        while (true) {
+            ServoEvent event{};
+            if (xQueueReceive(this_controller->servo_event_queue, &event, 100 / portTICK_PERIOD_MS) == pdTRUE) {
+                ESP_LOGI(TAG, "popped servo move event: steps=%lu, direction=%s, max_speed=%lu, acceleration=%lu, jerks=%lu", event.steps, event.direction ? "true" : "false", event.max_speed, event.acceleration, event.jerks);
+
+                if (this_controller->motors.empty()) {
+                    ESP_LOGW(TAG, "servo move received but no motors were configured");
+                    continue;
+                }
+
+                const uint32_t step_pulse_width_us = 10U;
+
+                const uint32_t total_steps = std::max<uint32_t>(1U, event.steps);
+                const uint32_t max_speed = std::max<uint32_t>(1U, event.max_speed);
+                const uint32_t acceleration = std::max<uint32_t>(1U, event.acceleration);
+                const uint32_t jerks = std::max<uint32_t>(1U, event.jerks);
+
+                ServoMotor* motorToUse = nullptr;
+                for (ServoMotor* motor : this_controller->motors) {
+                    if (motor == nullptr) {
+                        continue;
+                    }
+                    motorToUse = motor;
+                    break;
+                }
+
+                if (motorToUse == nullptr) {
+                    ESP_LOGW(TAG, "No valid motors found for servo move");
+                    continue;
+                }
+                
+
+                motorToUse->dir_pin->digital_write(event.direction ? 1 : 0);
+                if (motorToUse->enable_pin != nullptr) {
+                    motorToUse->enable_pin->digital_write(1);
+                    delay(1000); // wait 1000 ms for enable to take effect
+                }
+
+                uint32_t accelerate_steps = total_steps;
+                ESP_LOGI(TAG, "servo move:  accelerate_steps=%lu",  accelerate_steps);
+
+
+                uint32_t timeSpent = 0;
+                uint32_t lastPeriod = (uint32_t)((1.0f / 10) * 1000000.0f);
+                float currentstepsPerSecond = 10;
+                bool isAccelerating = true;
+                for (uint32_t step_index = 0; step_index < total_steps; ++step_index) {
+                    if(currentstepsPerSecond < (float)max_speed && isAccelerating) {
+                        // currentstepsPerSecond = (timeSpent / 1'000'000.0f) * acceleration;
+                        currentstepsPerSecond = currentstepsPerSecond + (float)((lastPeriod / 1'000'000.0f) * acceleration);
+                        accelerate_steps = step_index;
+                    } else if(step_index >= total_steps - accelerate_steps) {
+                        currentstepsPerSecond = currentstepsPerSecond - (float)((lastPeriod / 1'000'000.0f) * acceleration);
+                    } else {
+                        currentstepsPerSecond = (float)max_speed;
+                        isAccelerating = false;
+                    }
+
+                    if(currentstepsPerSecond < 10) {
+                        currentstepsPerSecond = 10;
+                    }
+                    if(currentstepsPerSecond > (float)max_speed) {
+                        currentstepsPerSecond = (float)max_speed;
+                    }
+
+                    int32_t period = (int32_t)((1.0f / currentstepsPerSecond) * 1000000.0f);
+                    // timeSpent += period;
+                    lastPeriod = period;
+                    if(period < step_pulse_width_us * 2) {
+                        ESP_LOGW(TAG, "pulse width too short : period=%ld, currentstepsPerSecond=%f", period, currentstepsPerSecond);
+
+                        period = step_pulse_width_us * 2;
+                    }
+
+                    motorToUse->step_pin->digital_write(1);
+                    delayMicroseconds(step_pulse_width_us);
+                    motorToUse->step_pin->digital_write(0);
+                    delayMicroseconds(period - step_pulse_width_us);
+                }
+
+                ESP_LOGI(TAG, "servo move done:  accelerate_steps=%lu",  accelerate_steps);
+
+                if (motorToUse->enable_pin != nullptr) {
+                    motorToUse->enable_pin->digital_write(0);
+                }
+            }
+        }
     }
 
 
-    void Servo_Garage_Door::dump_config()
-    {
-        ESP_LOGCONFIG(TAG, "Servo_Garage_Door reader:");
-    }
-
-    void Servo_Garage_Door::backgroundServoControlTask(void* params)
-    {
-
-    }
-    
-    void Servo_Garage_Door::update()
-    {
-
-    }
-
-} // namespace dc_relay
+} // namespace servo_garage_door
 } // namespace esphome
--- my_components/servo_garage_door/servo_garage_door.h
+++ my_components/servo_garage_door/servo_garage_door.h
@@ -2,10 +2,10 @@
 
 #include "esphome.h"
 // #include "esphome/components/button/button.h"
-#include "esphome/components/ledc/ledc_output.h"
+// #include "esphome/components/ledc/ledc_output.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
-#include "esphome/components/voltage_sampler/voltage_sampler.h"
+// #include "esphome/components/voltage_sampler/voltage_sampler.h"
 #include "esphome/core/automation.h"
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
@@ -14,18 +14,54 @@
 
 namespace esphome {
 namespace servo_garage_door {
-
-
-    class Servo_Garage_Door : public Component {
+    class ServoEvent {
     public:
+        uint32_t steps;
+        bool direction;
+        uint32_t max_speed;
+        uint32_t acceleration;
+        uint32_t jerks;
+    };
 
+    class ServoMotor;
 
+    class ServoGarageDoor : public Component {
+    public:
+        float get_setup_priority() const override { return setup_priority::LATE; }
+        void setup() override;
+        void dump_config() override;
+        // void set_motors(std::vector<ServoMotor*> _motors) { this->motors = std::move(_motors); };
+        void set_motors(std::vector<ServoMotor*> _motors) { this->motors = std::move(_motors); };
 
+        void move_servo(uint32_t steps, bool direction, uint32_t max_speed, uint32_t acceleration, uint32_t jerks);
     protected:
+
+        std::vector<ServoMotor*> motors;
 
         QueueHandle_t servo_event_queue;
         TaskHandle_t servo_task_handle { nullptr };
         static void backgroundServoControlTask(void* params);
+        
+    };
+
+
+    class ServoMotor : public Component {
+    public:
+        void setup() override;
+
+        GPIOPin* step_pin; // min pulse width 2us max frequency 300KHz
+        GPIOPin* dir_pin; // min pulse width 2us max frequency 300KHz
+        GPIOPin* enable_pin { nullptr }; // min pulse width 100us max frequency 10KHz
+        GPIOPin* alarm_pin { nullptr }; // open drain output, active low
+        GPIOPin* in_place_pin { nullptr }; // open drain output, active low
+        uint32_t steps_per_rotation;
+        
+        void set_Enable_pin(GPIOPin* pin) { this->enable_pin = pin; };
+        void set_Step_pin(GPIOPin* pin) { this->step_pin = pin; };
+        void set_Dir_pin(GPIOPin* pin) { this->dir_pin = pin; };
+        void set_Alarm_pin(GPIOPin* pin) { this->alarm_pin = pin; };
+        void set_In_Place_pin(GPIOPin* pin) { this->in_place_pin = pin; };
+        void set_Steps_Per_Rotation(uint32_t steps) { this->steps_per_rotation = steps; };
 
     };
 
